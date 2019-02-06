9d6b15508bbc0a52c014bce30dd26f39015a70c0
Marek Bugla <buglamarek@gmail.com>
2019-02-06 11:48:25 +0100

    update configuration to support sylius 1.4

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -56,4 +56,5 @@ return [
     Setono\SyliusGiftCardPlugin\SetonoSyliusGiftCardPlugin::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true, 'test_cached' => true],
 ];
