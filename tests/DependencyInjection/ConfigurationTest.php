6cc08d140506b155c4a08cdc29b680be72f7918e
Roshyo <stefdecock68@gmail.com>
2020-03-03 11:11:29 +0100

    Making tests pass

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -8,10 +8,20 @@ use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
 use PHPUnit\Framework\TestCase;
 use Setono\SyliusGiftCardPlugin\DependencyInjection\Configuration;
 use Setono\SyliusGiftCardPlugin\Doctrine\ORM\GiftCardRepository;
+use Setono\SyliusGiftCardPlugin\Form\Type\GiftCardChannelConfigurationType;
+use Setono\SyliusGiftCardPlugin\Form\Type\GiftCardConfigurationImageType;
+use Setono\SyliusGiftCardPlugin\Form\Type\GiftCardConfigurationType;
 use Setono\SyliusGiftCardPlugin\Form\Type\GiftCardType;
 use Setono\SyliusGiftCardPlugin\Model\GiftCard;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardChannelConfiguration;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardChannelConfigurationInterface;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfiguration;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfigurationImage;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfigurationImageInterface;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfigurationInterface;
 use Setono\SyliusGiftCardPlugin\Model\GiftCardInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Sylius\Component\Resource\Factory\Factory;
 
@@ -42,6 +52,36 @@ final class ConfigurationTest extends TestCase
                         'controller' => ResourceController::class,
                         'repository' => GiftCardRepository::class,
                         'form' => GiftCardType::class,
+                        'factory' => Factory::class,
+                    ],
+                ],
+                'gift_card_configuration' => [
+                    'classes' => [
+                        'model' => GiftCardConfiguration::class,
+                        'interface' => GiftCardConfigurationInterface::class,
+                        'controller' => ResourceController::class,
+                        'repository' => EntityRepository::class,
+                        'form' => GiftCardConfigurationType::class,
+                        'factory' => Factory::class,
+                    ],
+                ],
+                'gift_card_configuration_image' => [
+                    'classes' => [
+                        'model' => GiftCardConfigurationImage::class,
+                        'interface' => GiftCardConfigurationImageInterface::class,
+                        'controller' => ResourceController::class,
+                        'repository' => EntityRepository::class,
+                        'form' => GiftCardConfigurationImageType::class,
+                        'factory' => Factory::class,
+                    ],
+                ],
+                'gift_card_channel_configuration' => [
+                    'classes' => [
+                        'model' => GiftCardChannelConfiguration::class,
+                        'interface' => GiftCardChannelConfigurationInterface::class,
+                        'controller' => ResourceController::class,
+                        'repository' => EntityRepository::class,
+                        'form' => GiftCardChannelConfigurationType::class,
                         'factory' => Factory::class,
                     ],
                 ],
