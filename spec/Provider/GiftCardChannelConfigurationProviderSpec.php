c094bef35821f3a8ad8df140cbe52b6efe108efe
Roshyo <stefdecock68@gmail.com>
2020-03-04 13:49:48 +0100

    Adding notNull assertion Adding tests

---
+++ spec/Provider/GiftCardChannelConfigurationProviderSpec.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Setono\SyliusGiftCardPlugin\Provider;
+
+use PhpSpec\ObjectBehavior;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardChannelConfiguration;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfiguration;
+use Setono\SyliusGiftCardPlugin\Provider\GiftCardChannelConfigurationProvider;
+use Setono\SyliusGiftCardPlugin\Provider\GiftCardChannelConfigurationProviderInterface;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Locale\Context\LocaleContextInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+final class GiftCardChannelConfigurationProviderSpec extends ObjectBehavior
+{
+    public function let(
+        RepositoryInterface $configurationRepository,
+        LocaleContextInterface $localeContext,
+        RepositoryInterface $localeRepository
+    ) {
+        $this->beConstructedWith($configurationRepository, $localeContext, $localeRepository);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldBeAnInstanceOf(GiftCardChannelConfigurationProvider::class);
+    }
+
+    public function it_implements_gift_card_channel_configuration_provider_interface(): void
+    {
+        $this->shouldImplement(GiftCardChannelConfigurationProviderInterface::class);
+    }
+
+    public function it_provides_configuration_from_channel_and_locale(
+        RepositoryInterface $configurationRepository,
+        ChannelInterface $channel,
+        LocaleInterface $locale
+    ): void {
+        $configuration = new GiftCardConfiguration();
+        $channelConfiguration = new GiftCardChannelConfiguration();
+        $channelConfiguration->setConfiguration($configuration);
+        $configurationRepository->findOneBy(['channel' => $channel, 'locale' => $locale])->willReturn($channelConfiguration);
+
+        $this->getConfiguration($channel, $locale)->shouldReturn($configuration);
+    }
+
+    public function it_provides_default_configuration_if_none_found(
+        RepositoryInterface $configurationRepository,
+        ChannelInterface $channel,
+        LocaleInterface $locale
+    ) {
+        $configurationRepository->findOneBy(['channel' => $channel, 'locale' => $locale])->willReturn(null);
+
+        $defaultConfiguration = new GiftCardConfiguration();
+        $defaultChannelConfiguration = new GiftCardChannelConfiguration();
+        $defaultChannelConfiguration->setConfiguration($defaultConfiguration);
+        $configurationRepository->findOneBy(['default' => true])->willReturn($defaultChannelConfiguration);
+
+        $this->getConfiguration($channel, $locale)->shouldReturn($defaultConfiguration);
+    }
+}
---
+++ tests/Security/GiftCardVoterTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Setono\SyliusGiftCardPlugin\Security;
+
+use PHPUnit\Framework\TestCase;
+use Setono\SyliusGiftCardPlugin\Model\GiftCard;
+use Setono\SyliusGiftCardPlugin\Security\GiftCardVoter;
+use Sylius\Component\Core\Model\AdminUser;
+use Sylius\Component\Core\Model\Customer;
+use Sylius\Component\Core\Model\ShopUser;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class GiftCardVoterTest extends TestCase
+{
+    public function testAdminCanSeeAllGiftCards(): void
+    {
+        $giftCard = new GiftCard();
+        $voter = new GiftCardVoter();
+        $user = new AdminUser();
+        $token = new UsernamePasswordToken($user, 'credentials', 'memory');
+
+        $this->assertSame(
+            Voter::ACCESS_GRANTED,
+            $voter->vote($token, $giftCard, [GiftCardVoter::READ])
+        );
+    }
+
+    public function testUsersCanSeeTheirGiftCards(): void
+    {
+        $giftCard = new GiftCard();
+        $voter = new GiftCardVoter();
+        $user = new ShopUser();
+        $customer = new Customer();
+        $user->setCustomer($customer);
+
+        $giftCard->setCustomer($customer);
+
+        $token = new UsernamePasswordToken($user, 'credentials', 'memory');
+
+        $this->assertSame(
+            Voter::ACCESS_GRANTED,
+            $voter->vote($token, $giftCard, [GiftCardVoter::READ])
+        );
+    }
+
+    public function testUsersCanSeeAnonymousGiftCards(): void
+    {
+        $giftCard = new GiftCard();
+        $voter = new GiftCardVoter();
+        $user = new ShopUser();
+        $customer = new Customer();
+        $user->setCustomer($customer);
+
+        $token = new UsernamePasswordToken($user, 'credentials', 'memory');
+
+        $this->assertSame(
+            Voter::ACCESS_GRANTED,
+            $voter->vote($token, $giftCard, [GiftCardVoter::READ])
+        );
+    }
+
+    public function testUsersCanNotSeeOtherGiftCards(): void
+    {
+        $giftCard = new GiftCard();
+        $voter = new GiftCardVoter();
+        $user = new ShopUser();
+        $customer1 = new Customer();
+        $user->setCustomer($customer1);
+
+        $customer2 = new Customer();
+        $giftCard->setCustomer($customer2);
+
+        $token = new UsernamePasswordToken($user, 'credentials', 'memory');
+
+        $this->assertSame(
+            Voter::ACCESS_DENIED,
+            $voter->vote($token, $giftCard, [GiftCardVoter::READ])
+        );
+    }
+}
