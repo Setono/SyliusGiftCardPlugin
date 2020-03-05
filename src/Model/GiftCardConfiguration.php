492d9c1f9d52be5e22c7d38422f615d03a40a644
Roshyo <stefdecock68@gmail.com>
2020-03-05 11:02:18 +0100

    Removing unused import

--- src/Model/GiftCardConfiguration.php
+++ src/Model/GiftCardConfiguration.php
@@ -7,7 +7,6 @@ namespace Setono\SyliusGiftCardPlugin\Model;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Core\Model\ImageInterface;
-use Sylius\Component\Resource\Model\CodeAwareInterface;
 use Sylius\Component\Resource\Model\TimestampableTrait;
 use Sylius\Component\Resource\Model\ToggleableTrait;
 
