7d8e9f92e12c1fa6fc3359b1f83c3e9b9d8e90cf
Joachim Løvgaard <joachim@loevgaard.dk>
2020-08-05 15:59:53 +0200

    Add translator to simplify template translations

--- src/EmailManager/GiftCardEmailManager.php
+++ src/EmailManager/GiftCardEmailManager.php
@@ -9,15 +9,20 @@ use Setono\SyliusGiftCardPlugin\Model\GiftCardInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Mailer\Sender\SenderInterface;
+use Symfony\Contracts\Translation\LocaleAwareInterface;
 
 final class GiftCardEmailManager implements GiftCardEmailManagerInterface
 {
     /** @var SenderInterface */
     private $sender;
 
-    public function __construct(SenderInterface $sender)
+    /** @var LocaleAwareInterface */
+    private $translator;
+
+    public function __construct(SenderInterface $sender, LocaleAwareInterface $translator)
     {
         $this->sender = $sender;
+        $this->translator = $translator;
     }
 
     public function sendEmailToCustomerWithGiftCard(CustomerInterface $customer, GiftCardInterface $giftCard): void
@@ -46,10 +51,25 @@ final class GiftCardEmailManager implements GiftCardEmailManagerInterface
             return;
         }
 
+        $channel = $order->getChannel();
+        if (null === $channel) {
+            return;
+        }
+
+        $defaultLocale = $channel->getDefaultLocale();
+        if (null === $defaultLocale) {
+            return;
+        }
+
+        $oldLocale = $this->translator->getLocale();
+        $this->translator->setLocale((string) $defaultLocale->getCode());
+
         $this->sender->send(
             Emails::GIFT_CARD_ORDER,
             [$email],
-            ['giftCards' => $giftCards, 'order' => $order]
+            ['giftCards' => $giftCards, 'order' => $order, 'channel' => $channel]
         );
+
+        $this->translator->setLocale($oldLocale);
     }
 }
