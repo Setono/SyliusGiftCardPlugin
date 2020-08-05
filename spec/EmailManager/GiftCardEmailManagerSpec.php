cfb8e3ddd54a9026c795d72643d7e26cec4b983d
Joachim Løvgaard <joachim@loevgaard.dk>
2020-08-05 16:40:28 +0200

    Fix spec

--- spec/EmailManager/GiftCardEmailManagerSpec.php
+++ spec/EmailManager/GiftCardEmailManagerSpec.php
@@ -9,15 +9,22 @@ use Setono\SyliusGiftCardPlugin\EmailManager\GiftCardEmailManager;
 use Setono\SyliusGiftCardPlugin\EmailManager\GiftCardEmailManagerInterface;
 use Setono\SyliusGiftCardPlugin\Mailer\Emails;
 use Setono\SyliusGiftCardPlugin\Model\GiftCardInterface;
+use Setono\SyliusGiftCardPlugin\Resolver\CustomerChannelResolverInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
 use Sylius\Component\Mailer\Sender\SenderInterface;
+use Symfony\Contracts\Translation\LocaleAwareInterface;
 
-final class GiftCardOrderEmailManagerSpec extends ObjectBehavior
+final class GiftCardEmailManagerSpec extends ObjectBehavior
 {
-    public function let(SenderInterface $sender): void
-    {
-        $this->beConstructedWith($sender);
+    public function let(
+        SenderInterface $sender,
+        LocaleAwareInterface $translator,
+        CustomerChannelResolverInterface $customerChannelResolver
+    ): void {
+        $this->beConstructedWith($sender, $translator, $customerChannelResolver);
     }
 
     public function it_is_initializable(): void
@@ -34,17 +41,22 @@ final class GiftCardOrderEmailManagerSpec extends ObjectBehavior
         OrderInterface $order,
         CustomerInterface $customer,
         GiftCardInterface $giftCard,
-        SenderInterface $sender
+        SenderInterface $sender,
+        ChannelInterface $channel,
+    LocaleInterface $locale
     ): void {
         $customer->getEmail()->willReturn('example@shop.com');
         $order->getCustomer()->willReturn($customer);
+        $order->getChannel()->willReturn($channel);
+        $channel->getDefaultLocale()->willReturn($locale);
+        $locale->getCode()->willReturn('en_US');
 
         $sender->send(
             Emails::GIFT_CARD_ORDER,
             ['example@shop.com'],
-            ['giftCards' => [$giftCard], 'order' => $order]
+            ['giftCards' => [$giftCard], 'order' => $order, 'channel' => $channel]
         )->shouldBeCalled();
 
-        $this->sendEmailWithGiftCards($order, [$giftCard]);
+        $this->sendEmailWithGiftCardsFromOrder($order, [$giftCard]);
     }
 }
