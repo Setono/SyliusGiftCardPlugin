e1a743bbdf5eaea42d94fbf8deb4b0a3bfb6684d
Roshyo <stefdecock68@gmail.com>
2020-08-06 16:14:01 +0200

    Adding fixture for GiftCardConfiguration
    Fixes #81

---
+++ src/Fixture/Factory/GiftCardConfigurationExampleFactory.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusGiftCardPlugin\Fixture\Factory;
+
+use Setono\SyliusGiftCardPlugin\Factory\GiftCardConfigurationFactoryInterface;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfigurationImageInterface;
+use Setono\SyliusGiftCardPlugin\Model\GiftCardConfigurationInterface;
+use Sylius\Bundle\CoreBundle\Fixture\Factory\AbstractExampleFactory;
+use Sylius\Bundle\CoreBundle\Fixture\Factory\ExampleFactoryInterface;
+use Sylius\Component\Core\Uploader\ImageUploaderInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GiftCardConfigurationExampleFactory extends AbstractExampleFactory implements ExampleFactoryInterface
+{
+    /** @var RepositoryInterface */
+    protected $giftCardConfigurationRepository;
+
+    /** @var GiftCardConfigurationFactoryInterface */
+    protected $giftCardConfigurationFactory;
+
+    /** @var FactoryInterface */
+    private $imageFactory;
+
+    /** @var ImageUploaderInterface */
+    private $imageUploader;
+
+    /** @var FileLocatorInterface */
+    private $fileLocator;
+
+    /** @var OptionsResolver */
+    protected $optionsResolver;
+
+    public function __construct(
+        RepositoryInterface $giftCardConfigurationRepository,
+        GiftCardConfigurationFactoryInterface $giftCardConfigurationFactory,
+        FactoryInterface $imageFactory,
+        ImageUploaderInterface $imageUploader,
+        FileLocatorInterface $fileLocator
+    ) {
+        $this->giftCardConfigurationRepository = $giftCardConfigurationRepository;
+        $this->giftCardConfigurationFactory = $giftCardConfigurationFactory;
+        $this->imageFactory = $imageFactory;
+        $this->imageUploader = $imageUploader;
+        $this->fileLocator = $fileLocator;
+
+        $this->optionsResolver = new OptionsResolver();
+
+        $this->configureOptions($this->optionsResolver);
+    }
+
+    public function create(array $options = []): GiftCardConfigurationInterface
+    {
+        $options = $this->optionsResolver->resolve($options);
+
+        return $this->createGiftCardConfiguration($options);
+    }
+
+    protected function createGiftCardConfiguration(array $options): GiftCardConfigurationInterface
+    {
+        /** @var GiftCardConfigurationInterface|null $giftCardConfiguration */
+        $giftCardConfiguration = $this->giftCardConfigurationRepository->findOneBy(['code' => $options['code']]);
+        if (null === $giftCardConfiguration) {
+            $giftCardConfiguration = $this->giftCardConfigurationFactory->createNew();
+        }
+
+        $giftCardConfiguration->setCode($options['code']);
+        $giftCardConfiguration->setEnabled($options['enabled']);
+        $giftCardConfiguration->setDefault($options['default_configuration']);
+
+        $imagePath = $options['background_image'];
+
+        $imagePath = $this->fileLocator === null ? $imagePath : $this->fileLocator->locate($imagePath);
+        $uploadedImage = new UploadedFile($imagePath, basename($imagePath));
+
+        /** @var GiftCardConfigurationImageInterface $image */
+        $image = $this->imageFactory->createNew();
+        $image->setFile($uploadedImage);
+
+        $this->imageUploader->upload($image);
+
+        $giftCardConfiguration->setBackgroundImage($image);
+
+        return $giftCardConfiguration;
+    }
+
+    protected function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver
+            ->setDefault('code', null)
+            ->setAllowedTypes('code', 'string')
+
+            ->setDefault('background_image', null)
+            ->setAllowedTypes('background_image', 'string')
+
+            ->setDefault('enabled', true)
+            ->setAllowedTypes('enabled', 'bool')
+
+            ->setDefault('default_configuration', false)
+            ->setAllowedTypes('default_configuration', 'bool')
+        ;
+    }
+}
---
+++ src/Fixture/GiftCardConfigurationFixture.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusGiftCardPlugin\Fixture;
+
+use Sylius\Bundle\CoreBundle\Fixture\AbstractResourceFixture;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class GiftCardConfigurationFixture extends AbstractResourceFixture
+{
+    public function getName(): string
+    {
+        return 'setono_gift_card_configuration';
+    }
+
+    protected function configureResourceNode(ArrayNodeDefinition $resourceNode): void
+    {
+        $resourceNode
+            ->children()
+                ->scalarNode('code')->cannotBeEmpty()->end()
+                ->scalarNode('background_image')->cannotBeEmpty()->end()
+                ->booleanNode('enabled')->end()
+                ->booleanNode('default_configuration')->end()
+        ;
+    }
+}
