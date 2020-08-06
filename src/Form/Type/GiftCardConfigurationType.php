a03bae4b0f65825e50d76a7fc6c2ec46b37a9d98
Roshyo <stefdecock68@gmail.com>
2020-08-06 21:06:42 +0200

    Removing useless form configuration

--- src/Form/Type/GiftCardConfigurationType.php
+++ src/Form/Type/GiftCardConfigurationType.php
@@ -9,8 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class GiftCardConfigurationType extends AbstractResourceType
 {
@@ -44,19 +42,5 @@ final class GiftCardConfigurationType extends AbstractResourceType
     public function getBlockPrefix(): string
     {
         return 'setono_sylius_gift_card_gift_card_configuration';
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        parent::configureOptions($resolver);
-        $resolver->setDefault('validation_groups', function (FormInterface $form): array {
-            $validationGroups = $this->validationGroups;
-            $data = $form->getData();
-            if (null === $data->getId()) {
-                $validationGroups[] = 'setono_sylius_gift_card_create';
-            }
-
-            return $validationGroups;
-        });
     }
 }
