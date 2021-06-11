7a3d600eb39a4d5bace4e1ac01879b74628fafaf
Joachim Løvgaard <joachim@loevgaard.dk>
2021-06-11 16:21:33 +0200

    Up the type robustness

--- src/Resolver/RedirectUrlResolver.php
+++ src/Resolver/RedirectUrlResolver.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Setono\SyliusGiftCardPlugin\Resolver;
 
-use function array_key_exists;
 use const FILTER_SANITIZE_URL;
 use function filter_var;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,21 +20,24 @@ final class RedirectUrlResolver implements RedirectUrlResolverInterface
 
     public function getUrlToRedirectTo(Request $request, string $defaultRoute): string
     {
-        if ($request->attributes->has('redirect')) {
-            $redirect = $request->attributes->get('redirect');
-
-            if (array_key_exists(0, $redirect)) {
+        /** @var mixed $redirect */
+        $redirect = $request->attributes->get('redirect');
+        if (is_array($redirect)) {
+            if (isset($redirect[0]) && is_string($redirect[0])) {
                 return $this->router->generate($redirect[0]);
             }
-            if (array_key_exists('route', $redirect) && array_key_exists('parameters', $redirect)) {
+
+            if (isset($redirect['route'], $redirect['parameters']) && is_string($redirect['route']) && is_array($redirect['parameters'])) {
                 return $this->router->generate($redirect['route'], $redirect['parameters']);
             }
         }
 
-        if ($request->headers->has('referer')) {
-            $redirectTo = filter_var($request->headers->get('referer'), FILTER_SANITIZE_URL);
+        $referrer = $request->headers->get('referer');
+        if (is_string($referrer)) {
+            /** @var mixed $redirectTo */
+            $redirectTo = filter_var($referrer, FILTER_SANITIZE_URL);
 
-            if (false !== $redirectTo) {
+            if (is_string($redirectTo)) {
                 return $redirectTo;
             }
         }
