99ae4160ba38e7be2076716c56fde2030d6483b8
Roshyo <stefdecock68@gmail.com>
2021-08-27 09:46:41 +0200

    Fix CS and Psalm

--- src/Api/Controller/Action/ResendGiftCardEmailAction.php
+++ src/Api/Controller/Action/ResendGiftCardEmailAction.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Setono\SyliusGiftCardPlugin\Api\Controller\Action;
 
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Setono\SyliusGiftCardPlugin\EmailManager\GiftCardEmailManagerInterface;
 use Setono\SyliusGiftCardPlugin\Model\GiftCardInterface;
 use Setono\SyliusGiftCardPlugin\Model\OrderInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 final class ResendGiftCardEmailAction
 {
@@ -23,10 +23,10 @@ final class ResendGiftCardEmailAction
     public function __invoke(GiftCardInterface $data): Response
     {
         $giftCard = $data;
-        if ($giftCard->getOrder() instanceof OrderInterface) {
-            $this->giftCardEmailManager->sendEmailWithGiftCardsFromOrder($giftCard->getOrder(), [$giftCard]);
-        } elseif ($giftCard->getCustomer() instanceof CustomerInterface) {
-            $this->giftCardEmailManager->sendEmailToCustomerWithGiftCard($giftCard->getCustomer(), $giftCard);
+        if (($order = $giftCard->getOrder()) instanceof OrderInterface) {
+            $this->giftCardEmailManager->sendEmailWithGiftCardsFromOrder($order, [$giftCard]);
+        } elseif (($customer = $giftCard->getCustomer()) instanceof CustomerInterface) {
+            $this->giftCardEmailManager->sendEmailToCustomerWithGiftCard($customer, $giftCard);
         } else {
             throw new BadRequestHttpException();
         }
