ef06468c2a733fc751cd3ea8322ebcbe791f2485
Roshyo <stefdecock68@gmail.com>
2021-09-04 11:54:46 +0200

    Fix un-used imports

--- tests/Behat/Context/Api/Shop/ManagingGiftCardsContext.php
+++ tests/Behat/Context/Api/Shop/ManagingGiftCardsContext.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Setono\SyliusGiftCardPlugin\Behat\Context\Api\Shop;
 
-use ApiPlatform\Core\Api\IriConverterInterface;
 use Behat\Behat\Context\Context;
 use Sylius\Behat\Client\ApiClientInterface;
 use Sylius\Behat\Client\ResponseCheckerInterface;
-use Sylius\Component\Core\Model\ChannelInterface;
-use Sylius\Component\Core\Model\CustomerInterface;
-use Sylius\Component\Currency\Model\CurrencyInterface;
 use Webmozart\Assert\Assert;
 
 final class ManagingGiftCardsContext implements Context
@@ -19,16 +15,12 @@ final class ManagingGiftCardsContext implements Context
 
     private ResponseCheckerInterface $responseChecker;
 
-    private IriConverterInterface $iriConverter;
-
     public function __construct(
         ApiClientInterface $client,
-        ResponseCheckerInterface $responseChecker,
-        IriConverterInterface $iriConverter
+        ResponseCheckerInterface $responseChecker
     ) {
         $this->client = $client;
         $this->responseChecker = $responseChecker;
-        $this->iriConverter = $iriConverter;
     }
 
     /**
