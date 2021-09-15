eb476033161c1e75e31c0d257d28030dddd581f2
Roshyo <stefdecock68@gmail.com>
2021-09-15 08:23:53 +0200

    Add missing tests for GiftCardInformation.
    Remove spec for Operator

--- tests/Unit/Order/GiftCardInformationTest.php
+++ tests/Unit/Order/GiftCardInformationTest.php
@@ -19,4 +19,24 @@ final class GiftCardInformationTest extends TestCase
         $this->assertEquals($giftCardInformation->getAmount(), 80);
         $this->assertEquals($giftCardInformation->getCustomMessage(), 'message');
     }
+
+    /**
+     * @test
+     */
+    public function it_has_settable_amount(): void
+    {
+        $giftCardInformation = new GiftCardInformation(80, 'message');
+        $giftCardInformation->setAmount(200);
+        $this->assertEquals($giftCardInformation->getAmount(), 200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_settable_message(): void
+    {
+        $giftCardInformation = new GiftCardInformation(80, 'message');
+        $giftCardInformation->setCustomMessage('new message');
+        $this->assertEquals($giftCardInformation->getCustomMessage(), 'new message');
+    }
 }
