afd65ca71334c27db1201548c46a839bd899da51
Roshyo <stefdecock68@gmail.com>
2022-01-17 12:26:19 +0100

    Add blank line between constants

--- src/Provider/PdfOrientationProviderInterface.php
+++ src/Provider/PdfOrientationProviderInterface.php
@@ -7,6 +7,7 @@ namespace Setono\SyliusGiftCardPlugin\Provider;
 interface PdfOrientationProviderInterface
 {
     public const ORIENTATION_PORTRAIT = 'Portrait';
+
     public const ORIENTATION_LANDSCAPE = 'Landscape';
 
     public function getAvailableOrientations(): array;
--- src/Provider/PdfPageSizeProviderInterface.php
+++ src/Provider/PdfPageSizeProviderInterface.php
@@ -7,10 +7,15 @@ namespace Setono\SyliusGiftCardPlugin\Provider;
 interface PdfPageSizeProviderInterface
 {
     public const FORMAT_A3 = 'A3';
+
     public const FORMAT_A4 = 'A4';
+
     public const FORMAT_A5 = 'A5';
+
     public const FORMAT_A6 = 'A6';
+
     public const FORMAT_A7 = 'A7';
+
     public const FORMAT_A8 = 'A8';
 
     public function getAvailablePageSizes(): array;
