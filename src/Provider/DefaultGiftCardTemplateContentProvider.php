5ff29a70603cd49fe5817925ac35a0ea74456d94
Joachim Løvgaard <joachim@loevgaard.dk>
2022-05-09 15:46:17 +0200

    WIP

--- src/Provider/DefaultGiftCardTemplateContentProvider.php
+++ src/Provider/DefaultGiftCardTemplateContentProvider.php
@@ -9,14 +9,31 @@ final class DefaultGiftCardTemplateContentProvider implements DefaultGiftCardTem
     public function getContent(): string
     {
         return <<<TWIG
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 <style>
 body {
     background: transparent url("{{ giftCardConfiguration.image }}") no-repeat left bottom;
     background-size: 100px;
+    font-family: 'Open Sans', sans-serif;
+}
+.amount {
+    font-size: 50px;
+    text-align: center;
+}
+.code {
+    font-size: 30px;
+    text-align: center;
+}
+.expires {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    color: #6b6b6b;
 }
 </style>
-<p class="code">{{ giftCard.code }}</p>
 <p class="amount">{{ giftCard.amount }}</p>
+<p class="code">{{ giftCard.code }}</p>
+<p class="expires">Expires: {{ giftCard.expiresAt|date }}</p>
 TWIG;
     }
 }
