527838e024f4a48e8a951c9a3008253b6ab9e8c9
Joachim Løvgaard <joachim@loevgaard.dk>
2026-07-03 16:30:31 +0200

    Prepend bundle configuration as PHP arrays instead of loading YAML files
    Per review: build the prepended configuration (winzou_state_machine, liip_imagine,
    sylius_mailer, sylius_ui, sylius_grid) as PHP arrays directly in the extension's
    prepend() and drop the src/Resources/config/prepend/*.yaml files it used to parse.
    Verified the resulting config is identical via container lint + debug:config and by
    rendering the admin grid, the shop gift card form, and the winzou callbacks.

--- src/DependencyInjection/SetonoSyliusGiftCardExtension.php
+++ src/DependencyInjection/SetonoSyliusGiftCardExtension.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Setono\SyliusGiftCardPlugin\DependencyInjection;
 
+use Setono\SyliusGiftCardPlugin\Operator\OrderGiftCardOperator;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\Yaml\Yaml;
 
 final class SetonoSyliusGiftCardExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
@@ -58,28 +58,314 @@ final class SetonoSyliusGiftCardExtension extends AbstractResourceExtension impl
      */
     public function prepend(ContainerBuilder $container): void
     {
-        $files = glob(__DIR__ . '/../Resources/config/prepend/*.yaml');
-        if (false === $files) {
-            return;
-        }
+        $operator = '@' . OrderGiftCardOperator::class;
 
-        foreach ($files as $file) {
-            /** @var mixed $parsed */
-            $parsed = Yaml::parseFile($file);
-            if (!is_array($parsed)) {
-                continue;
-            }
+        $configuration = [
+            'winzou_state_machine' => [
+                'sylius_order_checkout' => [
+                    'callbacks' => [
+                        'after' => [
+                            'setono_sylius_gift_card__reconcile_gift_cards' => [
+                                'on' => ['complete'],
+                                'do' => [$operator, 'reconcile'],
+                                'args' => ['object'],
+                            ],
+                        ],
+                    ],
+                ],
+                'sylius_order_payment' => [
+                    'callbacks' => [
+                        'after' => [
+                            'setono_sylius_gift_card__enable_gift_cards' => [
+                                'on' => ['pay'],
+                                'do' => [$operator, 'enable'],
+                                'args' => ['object'],
+                            ],
+                            'setono_sylius_gift_card__send_gift_cards' => [
+                                'on' => ['pay'],
+                                'do' => [$operator, 'send'],
+                                'args' => ['object'],
+                            ],
+                        ],
+                    ],
+                ],
+                'sylius_order' => [
+                    'callbacks' => [
+                        'after' => [
+                            'setono_sylius_gift_card__commit_redemption' => [
+                                'on' => ['create'],
+                                'do' => ['@setono_sylius_gift_card.redemption_method', 'commit'],
+                                'args' => ['object'],
+                            ],
+                            'setono_sylius_gift_card__rollback_redemption' => [
+                                'on' => ['cancel'],
+                                'do' => ['@setono_sylius_gift_card.redemption_method', 'rollback'],
+                                'args' => ['object'],
+                            ],
+                            'setono_sylius_gift_card__disable_gift_cards' => [
+                                'on' => ['cancel'],
+                                'do' => [$operator, 'disable'],
+                                'args' => ['object'],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'liip_imagine' => [
+                'filter_sets' => [
+                    'setono_sylius_gift_card_design_thumbnail' => [
+                        'filters' => [
+                            'thumbnail' => ['size' => [240, 152], 'mode' => 'inset'],
+                        ],
+                    ],
+                    'setono_sylius_gift_card_design_preview' => [
+                        'filters' => [
+                            'thumbnail' => ['size' => [1200, 1200], 'mode' => 'inset'],
+                        ],
+                    ],
+                ],
+            ],
+            'sylius_mailer' => [
+                'emails' => [
+                    'setono_sylius_gift_card__gift_card' => [
+                        'subject' => 'setono_sylius_gift_card.email.new_gift_card',
+                        'template' => '@SetonoSyliusGiftCardPlugin/email/gift_card.html.twig',
+                    ],
+                    'setono_sylius_gift_card__gift_cards_from_order' => [
+                        'subject' => 'setono_sylius_gift_card.email.your_gift_cards_you_bought_in_the_order',
+                        'template' => '@SetonoSyliusGiftCardPlugin/email/gift_cards_from_order.html.twig',
+                    ],
+                ],
+            ],
+            'sylius_ui' => [
+                'events' => [
+                    'sylius.shop.product.show.add_to_cart_form' => [
+                        'blocks' => [
+                            'setono_gift_card_information' => [
+                                'template' => '@SetonoSyliusGiftCardPlugin/shop/product/show/_gift_card_information.html.twig',
+                                'priority' => 10,
+                            ],
+                        ],
+                    ],
+                    'sylius.shop.cart.summary' => [
+                        'blocks' => [
+                            'setono_gift_cards' => [
+                                'template' => '@SetonoSyliusGiftCardPlugin/shop/cart/_gift_cards.html.twig',
+                                'priority' => 10,
+                            ],
+                        ],
+                    ],
+                    'sylius.shop.cart.summary.totals' => [
+                        'blocks' => [
+                            'setono_gift_card_totals' => [
+                                'template' => '@SetonoSyliusGiftCardPlugin/shop/cart/_gift_card_totals.html.twig',
+                                'priority' => 10,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'sylius_grid' => [
+                'grids' => [
+                    'setono_sylius_gift_card_admin_gift_card' => [
+                        'driver' => [
+                            'name' => 'doctrine/orm',
+                            'options' => [
+                                'class' => '%setono_sylius_gift_card.model.gift_card.class%',
+                                'repository' => [
+                                    'method' => 'createListQueryBuilder',
+                                ],
+                            ],
+                        ],
+                        'limits' => [100, 200, 500, 1000],
+                        'sorting' => [
+                            'createdAt' => 'desc',
+                        ],
+                        'fields' => [
+                            'code' => [
+                                'type' => 'string',
+                                'label' => 'sylius.ui.code',
+                                'sortable' => null,
+                            ],
+                            'customer' => [
+                                'type' => 'twig',
+                                'label' => 'sylius.ui.customer',
+                                'options' => [
+                                    'template' => '@SetonoSyliusGiftCardPlugin/admin/gift_card/grid/field/customer.html.twig',
+                                ],
+                            ],
+                            'amount' => [
+                                'type' => 'twig',
+                                'label' => 'sylius.ui.amount',
+                                'path' => '.',
+                                'options' => [
+                                    'template' => '@SetonoSyliusGiftCardPlugin/admin/gift_card/grid/field/amount.html.twig',
+                                ],
+                            ],
+                            'deliveryType' => [
+                                'type' => 'twig',
+                                'label' => 'setono_sylius_gift_card.ui.delivery_type',
+                                'path' => '.',
+                                'sortable' => null,
+                                'options' => [
+                                    'template' => '@SetonoSyliusGiftCardPlugin/admin/gift_card/grid/field/delivery_type.html.twig',
+                                ],
+                            ],
+                            'enabled' => [
+                                'type' => 'twig',
+                                'label' => 'sylius.ui.enabled',
+                                'options' => [
+                                    'template' => '@SyliusUi/Grid/Field/enabled.html.twig',
+                                ],
+                                'sortable' => null,
+                            ],
+                            'createdAt' => [
+                                'type' => 'datetime',
+                                'label' => 'sylius.ui.created_at',
+                                'sortable' => null,
+                                'options' => [
+                                    'format' => 'Y-m-d H:i',
+                                ],
+                            ],
+                        ],
+                        'filters' => [
+                            'code' => [
+                                'type' => 'string',
+                                'label' => 'sylius.ui.code',
+                            ],
+                            'enabled' => [
+                                'type' => 'boolean',
+                                'label' => 'sylius.ui.enabled',
+                            ],
+                        ],
+                        'actions' => [
+                            'main' => [
+                                'create' => [
+                                    'type' => 'create',
+                                ],
+                                'create_product' => [
+                                    'type' => 'default',
+                                    'label' => 'setono_sylius_gift_card.ui.create_gift_card_product',
+                                    'options' => [
+                                        'link' => [
+                                            'route' => 'setono_sylius_gift_card_admin_create_gift_card_product',
+                                        ],
+                                    ],
+                                    'icon' => 'shopping bag',
+                                ],
+                            ],
+                            'item' => [
+                                'show' => [
+                                    'type' => 'show',
+                                    'options' => [
+                                        'link' => [
+                                            'route' => 'setono_sylius_gift_card_admin_gift_card_show',
+                                            'parameters' => [
+                                                'id' => 'resource.id',
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                                'update' => [
+                                    'type' => 'update',
+                                ],
+                                'download_pdf' => [
+                                    'type' => 'default',
+                                    'label' => 'setono_sylius_gift_card.ui.download_pdf',
+                                    'options' => [
+                                        'link' => [
+                                            'route' => 'setono_sylius_gift_card_admin_gift_card_pdf',
+                                            'parameters' => [
+                                                'id' => 'resource.id',
+                                            ],
+                                        ],
+                                    ],
+                                    'icon' => 'download',
+                                ],
+                                'adjust_balance' => [
+                                    'type' => 'default',
+                                    'label' => 'setono_sylius_gift_card.ui.adjust_balance',
+                                    'options' => [
+                                        'link' => [
+                                            'route' => 'setono_sylius_gift_card_admin_gift_card_adjust_balance',
+                                            'parameters' => [
+                                                'id' => 'resource.id',
+                                            ],
+                                        ],
+                                    ],
+                                    'icon' => 'balance scale',
+                                ],
+                                'delete' => [
+                                    'type' => 'delete',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'setono_sylius_gift_card_admin_gift_card_design' => [
+                        'driver' => [
+                            'name' => 'doctrine/orm',
+                            'options' => [
+                                'class' => '%setono_sylius_gift_card.model.gift_card_design.class%',
+                            ],
+                        ],
+                        'sorting' => [
+                            'position' => 'asc',
+                        ],
+                        'fields' => [
+                            'image' => [
+                                'type' => 'twig',
+                                'label' => 'setono_sylius_gift_card.ui.image',
+                                'path' => '.',
+                                'options' => [
+                                    'template' => '@SetonoSyliusGiftCardPlugin/admin/gift_card_design/grid/field/image.html.twig',
+                                ],
+                            ],
+                            'code' => [
+                                'type' => 'string',
+                                'label' => 'sylius.ui.code',
+                                'sortable' => null,
+                            ],
+                            'name' => [
+                                'type' => 'string',
+                                'label' => 'sylius.ui.name',
+                                'path' => 'translation.name',
+                            ],
+                            'position' => [
+                                'type' => 'string',
+                                'label' => 'setono_sylius_gift_card.ui.position',
+                                'sortable' => null,
+                            ],
+                            'enabled' => [
+                                'type' => 'twig',
+                                'label' => 'sylius.ui.enabled',
+                                'options' => [
+                                    'template' => '@SyliusUi/Grid/Field/enabled.html.twig',
+                                ],
+                                'sortable' => null,
+                            ],
+                        ],
+                        'actions' => [
+                            'main' => [
+                                'create' => [
+                                    'type' => 'create',
+                                ],
+                            ],
+                            'item' => [
+                                'update' => [
+                                    'type' => 'update',
+                                ],
+                                'delete' => [
+                                    'type' => 'delete',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
 
-            /** @var mixed $config */
-            foreach ($parsed as $extension => $config) {
-                if (!is_string($extension) || !is_array($config)) {
-                    continue;
-                }
-
-                if (!$container->hasExtension($extension)) {
-                    continue;
-                }
-
+        foreach ($configuration as $extension => $config) {
+            if ($container->hasExtension($extension)) {
                 $container->prependExtensionConfig($extension, $config);
             }
         }
